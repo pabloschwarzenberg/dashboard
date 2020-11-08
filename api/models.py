7d64bc48322c760c4831425e6a5d05592f6a1ca1
pabloschwarzenberg <pabloschwarzenberg@gmail.com>
2020-11-08 14:03:41 -0300

    date

--- api/models.py
+++ api/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class PlayerStats(models.Model):
     player=models.CharField(max_length=10)
     session=models.CharField(max_length=36)
-    level=models.CharField(max_length=10)
+    level=models.CharField(max_length=36)
     action=models.IntegerField()
     time=models.FloatField()
     state=models.TextField()
+    timestamp=models.DateTimeField(auto_now_add=True)
